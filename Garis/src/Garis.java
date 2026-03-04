ba7641113bf23f381f102630c6e340be039714f3
SarifaAJ <sarifajusmar@gmail.com>
2026-03-04 23:02:30 +0700

    file tentang garis

---
+++ Garis/src/Garis.java
@@ -0,0 +1,9 @@
+/* Nama File    : Titik.java 
+ * Deskripsi    : berisi atribut dan method dalam class Titik
+ * Pembuat      : Sarifa Nuha Ardanti Jusmar
+ * Tanggal      : 4 Maret 2026
+ */
+
+public class Garis {
+    
+}
---
+++ Garis/src/MGaris.java
@@ -0,0 +1,11 @@
+/* Nama File    : Titik.java 
+ * Deskripsi    : berisi atribut dan method dalam class Titik
+ * Pembuat      : Sarifa Nuha Ardanti Jusmar
+ * Tanggal      : 4 Maret 2026
+ */
+
+public class MGaris {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
