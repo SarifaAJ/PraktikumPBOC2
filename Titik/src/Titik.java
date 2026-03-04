455544ce9fd494a48b2dd23ce84390d4b2542fa8
SarifaAJ <sarifajusmar@gmail.com>
2026-03-04 23:00:45 +0700

    file tentang titik

--- Titik/src/Titik.java
+++ Titik/src/Titik.java
@@ -1,9 +1,4 @@
-/* Nama File    : Titik.java 
- * Deskripsi    : berisi atribut dan method dalam class Titik
- * Pembuat      : Sarifa Nuha Ardanti Jusmar
- * Tanggal      : 4 Maret 2026
- */
-
+ 
 public class Titik {
     /* * * * * * ATRIBUT * * * * * */
     private double absis;
