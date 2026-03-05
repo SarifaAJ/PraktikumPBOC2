4c1b873a0ac341da3bacfcccc7f36a43c6cd149a
SarifaAJ <sarifajusmar@gmail.com>
2026-03-05 07:16:08 +0700

    file pertemuan2 tentang titik dan garis

--- Garis/src/Garis.java
+++ Garis/src/Garis.java
@@ -43,14 +43,38 @@ public class Garis {
         this.titikAkhir = titikAkhir;
     }
 
-    // Method untuk mendapatkan panjang garis
+    // mendapatkan panjang garis
     public double getPanjang() {
         return titikAwal.getJarak(titikAkhir);
     }
     
-    // Method untuk mendapatkan gradien garis
+    // mendapatkan gradien garis
     public double getGradien() {
         return (titikAkhir.getOrdinat() - titikAwal.getOrdinat()) / (titikAkhir.getAbsis() - titikAwal.getAbsis());
+    }
+
+    // mendapatkan titik tengah garis
+    public Titik getTitikTengah() {
+        double absisTengah = (titikAwal.getAbsis() + titikAkhir.getAbsis()) / 2;
+        double ordinatTengah = (titikAwal.getOrdinat() + titikAkhir.getOrdinat()) / 2;
+        return new Titik(absisTengah, ordinatTengah);
+    }
+
+    // apakah garis sejajar dengan garis lain
+    public boolean isSejajar(Garis garis) {
+        return this.getGradien() == garis.getGradien();
+    }
+
+    // apakah garis tegak lurus dengan garis lain
+    public boolean isTegakLurus(Garis garis) {
+        return this.getGradien() * garis.getGradien() == -1;
+    }
+
+    // persamaan garis dalam bentuk y = mx + c
+    public String getPersamaanGaris() {
+        double m = this.getGradien();
+        double c = titikAwal.getOrdinat() - m * titikAwal.getAbsis();
+        return "y = " + m + "x + " + c;
     }
 
     //mencetak garis
