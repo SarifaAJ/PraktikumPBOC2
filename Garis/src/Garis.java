c5da89d7b1a593324db140a75fc0923febf9626e
SarifaAJ <sarifajusmar@gmail.com>
2026-03-05 07:03:15 +0700

    file pertemuan 2 tentang titik dan garis

--- Garis/src/Garis.java
+++ Garis/src/Garis.java
@@ -1,3 +1,61 @@
+/* Nama File    : Garis.java 
+ * Deskripsi    : berisi atribut dan method dalam class Garis
+ * Pembuat      : Sarifa Nuha Ardanti Jusmar
+ * Tanggal      : 4 Maret 2026
+ */
+
 public class Garis {
+    private Titik titikAwal;
+    private Titik titikAkhir;
+    private static int counterGaris;
+
+    // Konstruktor tanpa parameter (membuat garis dari (0,0) ke (1,1))
+    public Garis() {
+        this.titikAwal = new Titik(0, 0);
+        this.titikAkhir = new Titik(1, 1);
+        counterGaris++;
+    }
+    
+    // Konstruktor dengan parameter Titik
+    public Garis(Titik titikAwal, Titik titikAkhir) {
+        this.titikAwal = titikAwal;
+        this.titikAkhir = titikAkhir;
+        counterGaris++;
+    }
+
+    public static int getCounterGaris() {
+        return counterGaris;
+    }
+
+    public Titik getTitikAwal() {
+        return titikAwal;
+    }
+    
+    public void setTitikAwal(Titik titikAwal) {
+        this.titikAwal = titikAwal;
+    }
+    
+    public Titik getTitikAkhir() {
+        return titikAkhir;
+    }
+    
+    public void setTitikAkhir(Titik titikAkhir) {
+        this.titikAkhir = titikAkhir;
+    }
+
+    // Method untuk mendapatkan panjang garis
+    public double getPanjang() {
+        return titikAwal.getJarak(titikAkhir);
+    }
+    
+    // Method untuk mendapatkan gradien garis
+    public double getGradien() {
+        return (titikAkhir.getOrdinat() - titikAwal.getOrdinat()) / (titikAkhir.getAbsis() - titikAwal.getAbsis());
+    }
+
+    //mencetak garis
+    public void printGaris() {
+        System.out.println("Garis dari " + titikAwal + " ke " + titikAkhir);
+    }
     
 }
--- Garis/src/MGaris.java
+++ Garis/src/MGaris.java
@@ -1,5 +1,20 @@
+/* Nama File    : MGaris.java 
+ * Deskripsi    : berisi atribut dan method dalam class Garis
+ * Pembuat      : Sarifa Nuha Ardanti Jusmar
+ * Tanggal      : 4 Maret 2026
+ */
+
 public class MGaris {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Garis G1 = new Garis();
+        G1.printGaris();
+
+        Titik T1 = new Titik(2,7);
+        G1.setTitikAwal(T1);
+
+        Titik T2 = new Titik(5,9);
+        G1.setTitikAkhir(T2);
+
+        
     }
 }
